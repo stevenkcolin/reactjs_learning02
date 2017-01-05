1cebdfd0e0c1dfae15fb49f8e9241d8f75001f65
chenlin <chenlin@gepingtech.com>
2017-01-05 08:12:25 +0800

    some example of reactJS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,52 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <h1>hello world!</h1>,
+  document.getElementById('div1')
+);
+
+// const element= <h1>hello world!</h1>;
+
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user= {
+  firstName: 'steve',
+  lastName: 'chen'
+};
+
+const element = (
+  <h1>hello, {formatName(user)}
+  </h1>
+);
+
+ReactDOM.render(
+  element,document.getElementById("div05")
+);
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ShoppingList name="mark"/>,
+document.getElementById("div06")
+)
